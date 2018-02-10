67e062ffc1b87f33377c0b3ddb5a65d359f94cbe
Allison Tarr <hello@allisontarr.com>
2018-02-09 21:02:00 -0500

    nix sidebar on page template (about page)

---
+++ themes/epic-child/page.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This template is used for the display of single pages.
+ */
+
+get_header(); ?>
+	<section class="content-wrapper page-content cf">
+		<article class="content cf">
+			<?php get_template_part( 'yoast', 'breadcrumbs' ); // Yoast Breadcrumbs ?>
+
+			<?php get_template_part( 'loop', 'page' ); // Loop - Page ?>
+		</article>
+	</section>
+
+<?php get_footer(); ?>
