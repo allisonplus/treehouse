f1d612933eb4bf5e67dec6a039d15b8c759b4c61
Allison Tarr <hello@allisontarr.com>
2018-02-09 21:30:23 -0500

    nix custom query + loop
    ❓Not sure...

--- themes/epic-child/includes/acf.php
+++ themes/epic-child/includes/acf.php
@@ -6,65 +6,9 @@
  */
 
 /**
- * Portfolio Query.
- *
- * @return WP_Query Portfolio items.
+ * Homepage heroine.
  */
-function treehouse_query_portfolio() {
-
-	return new WP_Query( array(
-		'post_type'              => 'portfolio',
-		'no_found_rows'          => true,
-		'update_post_meta_cache' => false,
-		'update_post_term_cache' => false,
-	) );
-}
-
-/**
- * 'Portfolio' Section
- */
-function treehouse_portfolio_gallery() {
-
-	// Stop if there's nothing to display.
-	if ( ! $images ) {
-		return false;
-	}
-
-	ob_start(); ?>
-
-	<?php $portfolio_pieces = treehouse_query_portfolio(); ?>
-
-	<section class="portfolio-pieces">
-
-	<?php while ( $portfolio_pieces->have_posts() ) : $portfolio_pieces->the_post(); ?>
-
-	<?php
-		$images = get_field( 'portfolio_images' );
-		$size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
-	?>
-
-		<?php if ( $images ) : ?>
-			<ul>
-				<?php foreach ( $images as $image ) : ?>
-					<li>
-						<?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		<?php endif; ?>
-
-	<?php endwhile; ?>
-
-	</section><!--.section-->
-
-	<?php
-	return ob_get_clean();
-}
-
-/**
- * Homepage slider.
- */
-function treehouse_homepage_focus() {
+function treehouse_homepage_heroine() {
 
 	$image = get_field( 'main_image' );
 
