91b66b9c11f272debb3ef0110f6f3d0325451e80
Allison Tarr <hello@allisontarr.com>
2018-02-20 22:33:00 -0500

    update gulp + sass workflow, separate out into partials
    📚archive portfolio: font weight updates
    👣footer: copyright display properties, background-colour adjustments
    ⏯️fix prev/next project buttons

--- themes/epic-child/gulpfile.js
+++ themes/epic-child/gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-	gulp.src('*.scss')
+	gulp.src('./assets/sass/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(''));
 });
 
 gulp.task('watch', function() {
-	gulp.watch('*.scss', ['styles']);
+	gulp.watch('./assets/sass/*.scss', ['styles']);
 });
