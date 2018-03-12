5eb182438f69eec65be8c8905dbbd5cfcfa713aa
Allison Tarr <hello@allisontarr.com>
2018-03-12 11:14:34 -0400

    add mu plugin info

--- mu-plugins/index.php
+++ mu-plugins/index.php
@@ -1,2 +1,10 @@
 <?php
+/**
+ * Plugin Name: Elena's Treehouse Portfolio Content
+ * Description: Data: Custom Post Types & ACF for Elena's Portfolio
+ * Version: 1.0.0
+ * Author: Allison Tarr
+ * Author URI: http://www.allisontarr.com
+ *
+ * */
 require WPMU_PLUGIN_DIR . '/treehouse-functionality/core-functionality.php';
